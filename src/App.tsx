3dfaf2cacb8328936b4e27f1e70edd819d8e0670
Aria Balaei <ariabalayi@gmail.com>
2024-07-06 16:52:23 +0330

    Functional Buttom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Button from "./components/Button";
-
+import Alert from "./components/Alert";
+import { useState } from "react";
 function App() {
-  
+  const [alertVisible, setAlertVisibility] = useState(false)
+
   return <div>
-    <Button onClick={() => console.log('Clicked')}>My Button</Button>
+    {alertVisible && <Alert onClose={()=> setAlertVisibility(false)}>Alart</Alert>}
+    <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
   </div>
 }
 
